a9ea9011a1823b383312bd40988610407f29ad5f
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-08-06 22:20:34 +0200

    Refactor Encoder and Decoder classes to Quasi-2D CNN implementation with updated architecture and tensor handling

--- packages/mhd_q2d_kae/src/mhd_q2d_kae/model.py
+++ packages/mhd_q2d_kae/src/mhd_q2d_kae/model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# mhd_canonical_kae/src/mhd_canonical_kae/model.py
+# packages/mhd_q2d_kae/src/mhd_q2d_kae/model.py
 
 from collections import OrderedDict
 
@@ -7,32 +7,36 @@ import torch
 import torch.nn as nn
 
 
-class Encoder(nn.Module):
+class EncoderQ2D(nn.Module):
     """
-    A 3D CNN Encoder that dynamically adapts to input spatial dimensions.
+    A Quasi-2D CNN Encoder. It treats the Y-dimension as part of the channel
+    information and performs 2D convolutions on the X-Z plane.
     """
 
     def __init__(
         self,
         in_channels: int,
+        y_dim: int,
         latent_dim: int,
     ):
         super().__init__()
         self.latent_dim = latent_dim
+        self.in_channels = in_channels
+        self.y_dim = y_dim
+
+        # The effective number of channels for the 2D convolution
+        effective_channels = self.in_channels * self.y_dim
 
         self.conv_network = nn.Sequential(
-            nn.Conv3d(in_channels, 32, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(effective_channels, 64, kernel_size=3, stride=2, padding=1),
             nn.GELU(),
-            nn.BatchNorm3d(32),
-            nn.Conv3d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.GELU(),
-            nn.BatchNorm3d(64),
-            nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
             nn.GELU(),
-            nn.BatchNorm3d(128),
-            nn.Conv3d(128, 256, kernel_size=3, stride=2, padding=1),
-            nn.GELU(),
-            nn.BatchNorm3d(256),
+            nn.BatchNorm2d(256),
         )
         
         self.fc_network = nn.Sequential(
@@ -44,53 +48,47 @@ class Encoder(nn.Module):
         self,
         x: torch.Tensor,
     ) -> torch.Tensor:
+        # Input shape: (B, C, X, Y, Z)
+        B, C, X, Y, Z = x.shape
+        
+        # Reshape to (B, C*Y, X, Z) for 2D convolution
+        x = x.permute(0, 1, 3, 2, 4).reshape(B, C * Y, X, Z)
+        
         x = self.conv_network(x)
         x = self.fc_network(x)
         return x
 
 
-class Decoder(nn.Module):
+class DecoderQ2D(nn.Module):
     """
-    A 3D CNN Decoder that dynamically adapts and crops its output.
+    A Quasi-2D CNN Decoder, symmetric to the Encoder.
     """
 
     def __init__(
         self,
         latent_dim: int,
         out_channels: int,
+        y_dim: int,
         encoder_flattened_size: int,
         conv_output_shape: tuple[int, ...],
-        target_spatial_dims: tuple[int, int, int],
     ):
         super().__init__()
         self.latent_dim = latent_dim
         self.out_channels = out_channels
+        self.y_dim = y_dim
         self.encoder_flattened_size = encoder_flattened_size
         self.conv_output_shape = conv_output_shape
-        self.target_spatial_dims = target_spatial_dims
 
         self.fc_network = nn.Linear(self.latent_dim, self.encoder_flattened_size)
 
         self.conv_transpose_network = nn.Sequential(
-            nn.ConvTranspose3d(
-                256, 128, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.GELU(),
-            nn.BatchNorm3d(128),
-            nn.ConvTranspose3d(
-                128, 64, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.GELU(),
-            nn.BatchNorm3d(64),
-            nn.ConvTranspose3d(
-                64, 32, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),
-            nn.GELU(),
-            nn.BatchNorm3d(32),
-            nn.ConvTranspose3d(
-                32, self.out_channels, kernel_size=3, stride=2, padding=1, output_padding=1
-            ),
-            # Add a Tanh activation to ensure output is in [-1, 1]
+            nn.BatchNorm2d(64),
+            nn.ConvTranspose2d(64, self.out_channels * self.y_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.Tanh(),
         )
 
@@ -102,16 +100,17 @@ class Decoder(nn.Module):
         x = x.view(-1, *self.conv_output_shape)
         x = self.conv_transpose_network(x)
         
-        # Crop the output to the target size internally.
-        s_d, s_h, s_w = self.target_spatial_dims
-        x = x[:, :, :s_d, :s_h, :s_w]
+        # Reshape back to 5D tensor: (B, C*Y, X, Z) -> (B, C, Y, X, Z) -> (B, C, X, Y, Z)
+        B, _, X, Z = x.shape
+        x = x.view(B, self.out_channels, self.y_dim, X, Z)
+        x = x.permute(0, 1, 3, 2, 4)
         
         return x
 
 
-class KoopmanAutoencoder(nn.Module):
+class KoopmanAutoencoderQ2D(nn.Module):
     """
-    The main Koopman Autoencoder model.
+    The main Quasi-2D Koopman Autoencoder model.
     """
 
     def __init__(
@@ -119,46 +118,40 @@ class KoopmanAutoencoder(nn.Module):
         in_channels: int,
         latent_dim: int,
         input_spatial_dims: tuple[int, int, int],
+        **kwargs,
     ):
         super().__init__()
-        self.encoder = Encoder(in_channels, latent_dim)
+        x_dim, y_dim, z_dim = input_spatial_dims
+        
+        self.encoder = EncoderQ2D(in_channels, y_dim, latent_dim)
         
         with torch.no_grad():
-            dummy_input = torch.zeros(1, in_channels, *input_spatial_dims)
-            conv_output = self.encoder.conv_network(dummy_input)
+            dummy_input = torch.zeros(1, in_channels, x_dim, y_dim, z_dim)
+            B, C, X, Y, Z = dummy_input.shape
+            dummy_reshaped = dummy_input.permute(0, 1, 3, 2, 4).reshape(B, C * Y, X, Z)
             
+            conv_output = self.encoder.conv_network(dummy_reshaped)
             flattened_size = conv_output.flatten(1).shape[1]
-            self.encoder.fc_network.add_module(
-                "1", nn.Linear(flattened_size, latent_dim)
-            )
+            self.encoder.fc_network.add_module("1", nn.Linear(flattened_size, latent_dim))
             conv_output_shape = conv_output.shape[1:]
 
-        self.decoder = Decoder(
+        self.decoder = DecoderQ2D(
             latent_dim=latent_dim,
             out_channels=in_channels,
+            y_dim=y_dim,
             encoder_flattened_size=flattened_size,
             conv_output_shape=conv_output_shape,
-            target_spatial_dims=input_spatial_dims,
         )
 
         self.koopman_operator = nn.Linear(latent_dim, latent_dim, bias=False)
 
-    def encode(
-        self,
-        x: torch.Tensor,
-    ) -> torch.Tensor:
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
         return self.encoder(x)
 
-    def decode(
-        self,
-        z: torch.Tensor,
-    ) -> torch.Tensor:
+    def decode(self, z: torch.Tensor) -> torch.Tensor:
         return self.decoder(z)
 
-    def koopman_step(
-        self,
-        z: torch.Tensor,
-    ) -> torch.Tensor:
+    def koopman_step(self, z: torch.Tensor) -> torch.Tensor:
         return self.koopman_operator(z)
 
     def forward(
@@ -171,6 +164,12 @@ class KoopmanAutoencoder(nn.Module):
         z_t_plus_1_predicted = self.koopman_step(z_t)
         x_t_reconstructed = self.decode(z_t)
         x_t_plus_1_predicted = self.decode(z_t_plus_1_predicted)
+
+        # Crop the output to match the input size if necessary.
+        if x_t_reconstructed.shape != x_t.shape:
+            s_x, s_y, s_z = x_t.shape[2:]
+            x_t_reconstructed = x_t_reconstructed[:, :, :s_x, :s_y, :s_z]
+            x_t_plus_1_predicted = x_t_plus_1_predicted[:, :, :s_x, :s_y, :s_z]
 
         return OrderedDict(
             [
