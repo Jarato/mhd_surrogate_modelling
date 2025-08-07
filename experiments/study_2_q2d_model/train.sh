bab6243892df0c61a5399b06e5f6fe1ee4f94d41
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-08-07 14:05:26 +0200

    testing successful

--- experiments/study_2_q2d_model/train.sh
+++ experiments/study_2_q2d_model/train.sh
@@ -18,10 +18,10 @@ NORM_STATS_PATH="/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_H
 # --- Hyperparameters to Sweep ---
 # To run a single test, just leave one value in each array.
 LEARNING_RATES=(1e-4)
-LATENT_DIMS=(128 256)
+LATENT_DIMS=(256)
 W_RECONS=(1.0)
 W_PREDS=(1.0)
-W_LINS=(1.0 10.0 50.0)
+W_LINS=(10.0)
 W_EIGS=(0.1)
 
 
@@ -36,7 +36,7 @@ for wl in "${W_LINS[@]}"; do
 for we in "${W_EIGS[@]}"; do
 
     RUN_NAME="lr_${lr}_ld_${ld}_wr_${wr}_wp_${wp}_wl_${wl}_we_${we}"
-    BASE_OUTPUT_DIR="output/param_sweep_04_07_25"
+    BASE_OUTPUT_DIR="output/test_runs"
     OUTPUT_DIR="${BASE_OUTPUT_DIR}/${RUN_NAME}"
     
     echo "--------------------------------------------------"
@@ -54,11 +54,11 @@ for we in "${W_EIGS[@]}"; do
       --w-pred "${wp}" \
       --w-lin "${wl}" \
       --w-eig "${we}" \
-      --epochs 128 \
+      --epochs  \
       --patience 20 \
       --lr-patience 8 \
       --clip-grad-value 25.0 \
-      --batch-size 8 \
+      --batch-size 16 \
       --lr-factor 0.1
       
     if [ $? -ne 0 ]; then
