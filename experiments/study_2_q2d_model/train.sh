8b8f3c0eddd87f26e19d4ee8f1863449d96d8345
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-08-07 16:02:12 +0200

    Update train.sh for hyperparameter sweep configuration and logging

--- experiments/study_2_q2d_model/train.sh
+++ experiments/study_2_q2d_model/train.sh
@@ -7,9 +7,9 @@
 # --- Configuration ---
 # This script should be run from the experiment directory, e.g.:
 # cd experiments/study_1_param_sweep
-# ./run_sweep.sh
+# ./train.sh
 # For logging use:
-# ./run_sweep.sh 2>&1 | tee output/sweep_log.txt
+# ./train.sh 2>&1 | tee output/log.txt
 
 TRAIN_SCRIPT="train.py"
 DATA_PATH="/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/interp/prep2/train_val_set.npz"
@@ -18,7 +18,7 @@ NORM_STATS_PATH="/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_H
 # --- Hyperparameters to Sweep ---
 # To run a single test, just leave one value in each array.
 LEARNING_RATES=(1e-4)
-LATENT_DIMS=(256)
+LATENT_DIMS=(1024)
 W_RECONS=(1.0)
 W_PREDS=(1.0)
 W_LINS=(10.0)
@@ -36,7 +36,7 @@ for wl in "${W_LINS[@]}"; do
 for we in "${W_EIGS[@]}"; do
 
     RUN_NAME="lr_${lr}_ld_${ld}_wr_${wr}_wp_${wp}_wl_${wl}_we_${we}"
-    BASE_OUTPUT_DIR="output/test_runs"
+    BASE_OUTPUT_DIR="output"
     OUTPUT_DIR="${BASE_OUTPUT_DIR}/${RUN_NAME}"
     
     echo "--------------------------------------------------"
@@ -54,11 +54,11 @@ for we in "${W_EIGS[@]}"; do
       --w-pred "${wp}" \
       --w-lin "${wl}" \
       --w-eig "${we}" \
-      --epochs  \
+      --epochs 3 \
       --patience 20 \
       --lr-patience 8 \
       --clip-grad-value 25.0 \
-      --batch-size 16 \
+      --batch-size 226 \
       --lr-factor 0.1
       
     if [ $? -ne 0 ]; then
