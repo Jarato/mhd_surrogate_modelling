c49b71fbbedab79e8141ae572258a9a18f400e69
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-08-08 23:05:14 +0200

    minor

--- experiments/study_2_q2d_model/run_sweep.py
+++ experiments/study_2_q2d_model/run_sweep.py
@@ -17,7 +17,7 @@ import logging
 TRAIN_SCRIPT = "train.py"
 DATA_PATH = "/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/interp/prep2/train_val_set.npz"
 NORM_STATS_PATH = "/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/interp/prep2/normalization_stats.npz"
-BASE_OUTPUT_DIR = Path("output")
+BASE_OUTPUT_DIR = Path("/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/run_mhd_surrogate_modelling/experiments/study_2_q2d_model/output")
 
 # --- Hyperparameter Grid ---
 # Define the parameter space for the grid search.
@@ -26,20 +26,21 @@ param_grid = {
     # Coupled parameters are defined as a list of tuples.
     # Each tuple is a complete set of (latent_dim, bottleneck_dim, batch_size).
     'model_params': [
-        (1024, 4096, 226),
-        (2048, 8192, 226),
-        # (4096, 16384, 226),
+        (4096, 4096, 226),
+        # (1024, 4096, 226),
+        # (2048, 8192, 151),
+        # (4096, 16384, 16),
     ],
     'w_recon': [1.0],
     'w_pred': [1.0],
-    'w_lin': [1.0, 10.0],
+    'w_lin': [1000000.0, 10000000.0, 100000000.0],
     'w_eig': [0.1],
 }
 
 # --- Fixed Training Arguments ---
 # These arguments will be the same for all runs.
 fixed_args = {
-    "epochs": 2,
+    "epochs": 128,
     "patience": 40,
     "lr_patience": 10,
     "clip_grad_value": 0.2,
