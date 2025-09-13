30d86360ba95741b05a6f382638ae440fb676aab
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-09-13 11:03:19 +0200

    ♻️ refactor(script): rename output directory
    - rename output directory from `all_time_evolution_plots` to `figures` for better clarity

--- data_plotting_and_stats/raw_dns_data/generate_all_time_evolution_plots.sh
+++ data_plotting_and_stats/raw_dns_data/generate_all_time_evolution_plots.sh
@@ -24,7 +24,7 @@ SNAPSHOT_DIR="/raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha32
 FILE_PREFIX="patt3d_vx3d_"
 
 # Directory to save the output plot images
-OUTPUT_DIR="output/all_time_evolution_plots"
+OUTPUT_DIR="output/figures"
 
 # Grid and Time parameters
 NX=2301
