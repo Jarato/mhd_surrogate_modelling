37c5e3ad2cd392b1c149ee3b71230fa5ab63e1d2
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-09-13 10:33:32 +0200

    🐛 fix(plot): adjust time end for complete plotting
    - adjust the time end from 625 to 737 to ensure complete plotting

--- data_plotting_and_stats/raw_dns_data/create_time_evolution_plots.py
+++ data_plotting_and_stats/raw_dns_data/create_time_evolution_plots.py
@@ -206,7 +206,7 @@ python create_time_evolution_plots.py \
     --snapshot-dir /raid/skowronek/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/raw/ \
     --output-dir output/figures/ \
     --nx 2301 --ny 481 --nz 121 \
-    --time-start 609 --time-end 625 \
+    --time-start 609 --time-end 737 \
     --plot-type time-x \
     --slice-indices 240 60 \
     --channel vy \
