50a72ea59ddc32b7b0ecc2d7e7775e3afe7e9b28
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-09-18 14:37:08 +0200

    minor

--- plotting_and_stats/2D/preprocessed_data/generate_all_videos_from_npz.sh
+++ plotting_and_stats/2D/preprocessed_data/generate_all_videos_from_npz.sh
@@ -19,7 +19,7 @@
 # --- USER CONFIGURATION ---
 
 # Path to the Python script that generates the video
-VIDEO_SCRIPT_PATH="create_video_from_npz_2d.py"
+VIDEO_SCRIPT_PATH="create_video_from_npz.py"
 
 # Path to the input 2D NPZ file
 INPUT_NPZ="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/data/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/T1492_x1151_y1_z127_c2/T1492_x1151_y1_z127_c2.npz"
