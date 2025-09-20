1b91cafcf27c9fe5aafce6df5fd2fdce2d0dd723
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-09-20 08:35:42 +0200

    ✨ feat(plotting): add processed plotting functions to xz module
    - add plot_xz_snapshot, plot_x_time_evolution, and plot_z_time_evolution
    - these functions allow for easy plotting of processed data

--- packages/mhd_surrogate_core/src/mhd_surrogate_core/plotting/xz/__init__.py
+++ packages/mhd_surrogate_core/src/mhd_surrogate_core/plotting/xz/__init__.py
@@ -3,7 +3,16 @@ The 2D (XZ) plotting package for the MHD Surrogate Core project.
 """
 
 from .video import generate_video_from_npz
+from .processed import (
+    plot_xz_snapshot,
+    plot_x_time_evolution,
+    plot_z_time_evolution,
+)
 
 __all__ = [
     "generate_video_from_npz",
+    "plot_xz_snapshot",
+    "plot_x_time_evolution",
+    "plot_z_time_evolution",
 ]
+
