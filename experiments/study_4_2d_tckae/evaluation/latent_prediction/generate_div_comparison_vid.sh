7190cfb93958d02a802cf86bdeea0bd545b0b4b2
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-09-26 10:41:33 +0200

    ✨ feat(latent_prediction): generate divergence comparison video
    - automatically construct GROUND_TRUTH_NPZ path
    - this is based on EVAL_DIR to improve script flexibility

--- experiments/study_4_2d_tckae/evaluation/latent_prediction/generate_div_comparison_vid.sh
+++ experiments/study_4_2d_tckae/evaluation/latent_prediction/generate_div_comparison_vid.sh
@@ -30,7 +30,7 @@ VIDEO_SCRIPT_PATH="create_comparison_video.py"
 EVAL_DIR="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/mhd_surrogate_modelling/experiments/study_4_2d_tckae/output/tc_size_exploration/ld512_M16_K16_Ktc16_gtc1.0_bwd_False/eval/latent_space_analysis/combined_high_energy_modes/divergence/"
 
 # The following paths are now constructed automatically based on EVAL_DIR
-GROUND_TRUTH_NPZ="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/data/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/T1492_x1151_y1_z127_c2/preprocessed/test_set.npz"
+GROUND_TRUTH_NPZ="${EVAL_DIR}/true_timeseries.npz"
 PREDICTED_NPZ="${EVAL_DIR}/predicted_timeseries.npz"
 DIFFERENCE_NPZ="${EVAL_DIR}/difference_timeseries.npz"
 
