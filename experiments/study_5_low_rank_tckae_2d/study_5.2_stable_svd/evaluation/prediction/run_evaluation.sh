853bb0f80ab9f474845e50d6a1208e70669d2211
MichaelSkowronek <michael.skowronek.91@gmail.com>
2025-11-06 20:52:05 +0100

    misc

--- experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/evaluation/prediction/run_evaluation.sh
+++ experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/evaluation/prediction/run_evaluation.sh
@@ -7,7 +7,7 @@
 # -----------------------
 
 # Path to your *best_model.pth* file
-MODEL_PATH="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/mhd_surrogate_modelling/experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/output/cnn_fc_suffix_layer/K16_val16_latent8192_koop16/ld8192_kr16_direct_M1_K16_Ktc0_gtc0_gi0.1_gfwd10.0_g_svdo0.0001_g_svdstab1e-05_bwd_False/best_model.pth"
+MODEL_PATH="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/mhd_surrogate_modelling/experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/output/force_recon/force_recon_K16_val16_koop16/flat_kr16_M1_K16_Ktc0_gtc0_gi10000.0_gfwd1.0_g_svdo0.0001_g_svdstab1e-05_bwd_False/best_model.pth"
 
 # Path to your *test_set.npz* file
 TEST_DATA_PATH="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/data/preprocessed_dns_output/01-Cold_Runs/01-Re16K_Ha325/T1492_x1151_y1_z127_c2/preprocessed/test_set.npz"
@@ -19,7 +19,7 @@ NORM_STATS_PATH="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/da
 
 # Directory where the output .npz files will be saved.
 # The analysis notebook will need to point to this directory.
-OUTPUT_DIR="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/mhd_surrogate_modelling/experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/output/cnn_fc_suffix_layer/K16_val16_latent8192_koop16/ld8192_kr16_direct_M1_K16_Ktc0_gtc0_gi0.1_gfwd10.0_g_svdo0.0001_g_svdstab1e-05_bwd_False/eval/pred/"
+OUTPUT_DIR="/cephfs/users/skowronek/Documents/PhD/nuclear_fusion_cooling/prediction/mhd_surrogate_modelling/experiments/study_5_low_rank_tckae_2d/study_5.2_stable_svd/output/force_recon/force_recon_K16_val16_koop16/flat_kr16_M1_K16_Ktc0_gtc0_gi10000.0_gfwd1.0_g_svdo0.0001_g_svdstab1e-05_bwd_False/eval/pred/"
 
 echo "Running SVD prediction generation..."
 
