04aeae65111d5fa25a5ad8f16b3dfe677b2a0251
Paul Fährmann <paulf@iesy.net>
2026-03-25 22:33:13 +0100

    rewrote model, set up big experiments

--- paul/autoenkoop-lib/src/autoenkoop/_model.py
+++ paul/autoenkoop-lib/src/autoenkoop/_model.py
@@ -133,71 +133,9 @@ class ConvAutoencoderZeroDecoder(nn.Module):
         z_t_plus_1 = self.linear_dynamics(z_t)
         x_t_plus_1 = self.decoder(z_t_plus_1)
         return x_t_plus_1
-    
-
-
-
-class ConvAutoencoderNorm(nn.Module):
-    def __init__(self, latent_dim, intermediate_dim = 1024, use_bias = True):
-        super().__init__()
-
-        self.latent_dimension = latent_dim
-        self.intermediate_dimension = intermediate_dim
-        self.flattened_dim = 73728
-        self.pre_2d_shape = (128, 72, 8)
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(128),
-            nn.Flatten(),
-            nn.Linear(self.flattened_dim, self.intermediate_dimension, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm1d(self.intermediate_dimension),
-            nn.Linear(self.intermediate_dimension, self.latent_dimension, bias=use_bias),
-        )
-
-        self.linear_dynamics = nn.Linear(self.latent_dimension, self.latent_dimension, bias=False)
-
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dimension, self.intermediate_dimension, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm1d(self.intermediate_dimension),
-            nn.Linear(self.intermediate_dimension, self.flattened_dim, bias=use_bias),
-            nn.GELU(), # REMOVE FOR OLD MODELS before 25.02.2026, 14:00
-            nn.Unflatten(1, self.pre_2d_shape),
-            nn.BatchNorm2d(128),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.ConvTranspose2d(16, 2, kernel_size=3, stride=2, padding=1, output_padding=0, bias=use_bias)
-        )
-
-    def forward(self, x_t):
-        z_t = self.encoder(x_t)
-        z_t_plus_1 = self.linear_dynamics(z_t)
-        x_t_plus_1 = self.decoder(z_t_plus_1)
-        return x_t_plus_1
-    
 
 class ConvAutoencoder(nn.Module):
-    def __init__(self, latent_dim, intermediate_dim = 1024, use_bias = True):
+    def __init__(self, latent_dim, intermediate_dim = 1024, bias_terms=True, norm_type="batch"): # "layer"
         super().__init__()
 
         self.latent_dimension = latent_dim
@@ -206,106 +144,54 @@ class ConvAutoencoder(nn.Module):
         self.pre_2d_shape = (128, 72, 8)
         # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=3, stride=2, padding=1, bias=use_bias),
+            nn.Conv2d(2, 16, kernel_size=3, stride=2, padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1, bias=use_bias),
+            nn.BatchNorm2d(16) if norm_type=="batch" else nn.GroupNorm(1, 16),
+            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=use_bias),
+            nn.BatchNorm2d(32) if norm_type=="batch" else nn.GroupNorm(1, 32),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=use_bias),
+            nn.BatchNorm2d(64) if norm_type=="batch" else nn.GroupNorm(1, 64),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128) if norm_type=="batch" else nn.GroupNorm(1, 128),
             nn.Flatten(),
-            nn.Linear(self.flattened_dim, self.intermediate_dimension, bias=use_bias),
+            nn.Linear(self.flattened_dim, self.intermediate_dimension, bias=bias_terms),
             nn.GELU(),
-            nn.Linear(self.intermediate_dimension, self.latent_dimension, bias=use_bias)
+            nn.BatchNorm1d(self.intermediate_dimension) if norm_type=="batch" else nn.LayerNorm(self.intermediate_dimension),
+            nn.Linear(self.intermediate_dimension, self.latent_dimension, bias=bias_terms),
         )
-
-        self.linear_dynamics = nn.Linear(self.latent_dimension, self.latent_dimension, bias=False)
 
         # Decoder
         self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dimension, self.intermediate_dimension, bias=use_bias),
+            nn.Linear(self.latent_dimension, self.intermediate_dimension, bias=bias_terms),
             nn.GELU(),
-            nn.Linear(self.intermediate_dimension, self.flattened_dim, bias=use_bias),
-            nn.GELU(), # REMOVE FOR OLD MODELS before 25.02.2026, 14:00
+            nn.BatchNorm1d(self.intermediate_dimension) if norm_type=="batch" else nn.LayerNorm(self.intermediate_dimension),
+            nn.Linear(self.intermediate_dimension, self.flattened_dim, bias=bias_terms),
+            nn.GELU(),
             nn.Unflatten(1, self.pre_2d_shape),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            nn.BatchNorm2d(128) if norm_type=="batch" else nn.GroupNorm(1, 128),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            nn.BatchNorm2d(64) if norm_type=="batch" else nn.GroupNorm(1, 64),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
+            nn.BatchNorm2d(32) if norm_type=="batch" else nn.GroupNorm(1, 32),
+            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1, bias=bias_terms),
             nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.ConvTranspose2d(16, 2, kernel_size=3, stride=2, padding=1, output_padding=0, bias=use_bias)
+            nn.BatchNorm2d(16) if norm_type=="batch" else nn.GroupNorm(1, 16),
+            nn.ConvTranspose2d(16, 2, kernel_size=3, stride=2, padding=1, output_padding=0, bias=bias_terms)
         )
+
+        self.linear_dynamics = nn.Linear(self.latent_dimension, self.latent_dimension, bias=bias_terms)
+
 
     def forward(self, x_t):
         z_t = self.encoder(x_t)
         z_t_plus_1 = self.linear_dynamics(z_t)
         x_t_plus_1 = self.decoder(z_t_plus_1)
         return x_t_plus_1
-    
-    
-class ConvAutoencoderKoop(nn.Module):
-    def __init__(self, latent_dim, intermediate_dim = 1024, use_bias = True):
-        super().__init__()
 
-        self.latent_dimension = latent_dim
-        self.intermediate_dimension = intermediate_dim
-        self.flattened_dim = 73728
-        self.pre_2d_shape = (128, 72, 8)
-        # Encoder
-        self.encoder = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(128),
-            nn.Flatten(),
-            nn.Linear(self.flattened_dim, self.intermediate_dimension, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm1d(self.intermediate_dimension),
-            nn.Linear(self.intermediate_dimension, self.latent_dimension, bias=use_bias),
-        )
 
-        #self.linear_dynamics = nn.Linear(self.latent_dimension, self.latent_dimension, bias=False)
 
-        # Decoder
-        self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dimension, self.intermediate_dimension, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm1d(self.intermediate_dimension),
-            nn.Linear(self.intermediate_dimension, self.flattened_dim, bias=use_bias),
-            nn.GELU(), # REMOVE FOR OLD MODELS before 25.02.2026, 14:00
-            nn.Unflatten(1, self.pre_2d_shape),
-            nn.BatchNorm2d(128),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(32),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            nn.GELU(),
-            nn.BatchNorm2d(16),
-            nn.ConvTranspose2d(16, 2, kernel_size=3, stride=2, padding=1, output_padding=0, bias=use_bias)
-        )
-
-    def forward(self, x_t):
-        z_t = self.encoder(x_t)
-        #z_t_plus_1 = self.linear_dynamics(z_t)
-        x_t_recon = self.decoder(z_t)
-        return z_t, x_t_recon
--- paul/experiments/kae_experiments.py
+++ paul/experiments/kae_experiments.py
@@ -20,7 +20,9 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 def calculate_linear_weights(model, lift_loader, loader):#, t_steps, latent_dim):
     Z = []#np.zeros((t_steps, latent_dim))
+    #mse = nn.MSELoss()
     latent_dim = model.latent_dimension
+    model.eval()
     with torch.no_grad():
         for x_t in lift_loader:
             x_t = x_t.to(DEVICE)
@@ -30,20 +32,22 @@ def calculate_linear_weights(model, lift_loader, loader):#, t_steps, latent_dim)
             Z.append(z_t.cpu().numpy())
 
     #num_train_examples = len(loader.dataset)
-    #linearity_loss_epoch = 0.0
+    #print("num train examples in data_loader", num_train_examples)
+    #linearity_loss_sum = 0.0
     #with torch.no_grad():
     #    for x_t, x_tp1 in loader:
-    #        x_t = x_t.to(DEVICE)
-    #        x_tp1 = x_tp1.to(DEVICE)
+    #        x_t, x_tp1 = x_t.to(DEVICE, non_blocking=True), x_tp1.to(DEVICE, non_blocking=True)
+    #        torch.cuda.synchronize()
+    #
     #        batch_size = x_t.size(0)
     #        #encoded current state
     #        z_t = model.encoder(x_t)
     #        z_tp1 = model.encoder(x_tp1)
     #        pz_tp1 = model.linear_dynamics(z_t)
-    #        
-    #        linearity_loss_epoch += mse(z_tp1, pz_tp1).item() * batch_size
-    #
-    #linearity_loss_mean = linearity_loss_epoch / num_train_examples
+    ##        
+    #        linearity_loss_sum += mse(z_tp1, pz_tp1).item() * batch_size
+    
+    #linearity_loss_mean = linearity_loss_sum / num_train_examples
 
     Z_all = np.vstack(Z).T
 
@@ -53,42 +57,36 @@ def calculate_linear_weights(model, lift_loader, loader):#, t_steps, latent_dim)
     dmd = pydmd.DMD(-1, forward_backward=False)
     dmd.fit(Z_all)
 
-    #Z0_all = np.vstack(Z0).T
-    #Z1_all = np.vstack(Z1).T
-    #K_star = Z1_all @ np.linalg.pinv(Z0_all)
     K_star = (dmd.modes @ np.diag(dmd.eigs) @ np.linalg.pinv(dmd.modes)).real
     with torch.no_grad():
         K = model.linear_dynamics.weight.cpu().detach().numpy()
-    
+
     optimal_residual = Z1_all - K_star @ Z0_all
     model_residual = Z1_all - K @ Z0_all
 
     num_data = Z0_all.shape[1] * latent_dim
     
-
-    true_latent_linearity_error = np.linalg.norm(optimal_residual)**2 / num_data
+    irreducible_linearity_error = np.linalg.norm(optimal_residual)**2 / num_data
     total_linearity_error = np.linalg.norm(model_residual)**2 / num_data
-    #print()
-    #print(model_residual.shape)
-    #print(num_data)
-    #print(total_linearity_error)
-    #print(linearity_loss_mean)
-    #print(linearity_loss_mean/total_linearity_error)
-    return K_star, Z_all, total_linearity_error, true_latent_linearity_error
+
+    model.train()
+    return K_star, total_linearity_error, irreducible_linearity_error
 
 def train_model(model, data_loader, lifting_data_loader, num_epochs):
     model.train()
     num_train_examples = len(data_loader.dataset)
+    
     mse = nn.MSELoss()
 
     train_history = {}
     train_history["loss_reconstruction"] = np.zeros((num_epochs))
     train_history["loss_linearity"] = np.zeros((num_epochs))
+    train_history["irreducible_lin_error"] = np.zeros((num_epochs))
+    train_history["estimation_lin_error"] = np.zeros((num_epochs))
     train_history["lr"] = np.zeros((num_epochs))
-    train_history["lifted_states"] = np.zeros((num_epochs, len(lifting_data_loader.dataset), model.latent_dimension))
     train_history["K"] = np.zeros((num_epochs, model.latent_dimension, model.latent_dimension))
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=10, cooldown=5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.8, patience=10, cooldown=5)
     
     epoch_progress = tqdm(range(num_epochs), dynamic_ncols=True)
 
@@ -96,16 +94,15 @@ def train_model(model, data_loader, lifting_data_loader, num_epochs):
         reconstruction_loss_epoch = 0.0
         linearity_loss_epoch = 0.0
 
-        lin_weights, Z, total_linearity_error, true_latent_linearity_error = calculate_linear_weights(model, lifting_data_loader, data_loader)
+        lin_weights, total_linearity_error, irreducible_linearity_error = calculate_linear_weights(model, lifting_data_loader, data_loader)
         lin_weights = torch.tensor(lin_weights, device=DEVICE, requires_grad=True)
-        train_history["lifted_states"][epoch] = Z.T
 
         for x_t, x_t_plus_1 in data_loader:
             x_t, x_t_plus_1 = x_t.to(DEVICE, non_blocking=True), x_t_plus_1.to(DEVICE, non_blocking=True)
             torch.cuda.synchronize()
             batch_size = x_t.size(0)
 
-            optimizer.zero_grad()
+            optimizer.zero_grad(set_to_none=True)
 
             #encoded current state
             z_t = model.encoder(x_t)
@@ -139,11 +136,9 @@ def train_model(model, data_loader, lifting_data_loader, num_epochs):
             optimizer.step()
 
             reconstruction_loss_epoch += loss_reconstruction.item() * batch_size
-            #prediction_loss_epoch += loss_prediction.item() * batch_size
             linearity_loss_epoch += loss_linearity.item() * batch_size
 
         reconstruction_loss_mean = reconstruction_loss_epoch / num_train_examples
-        #prediction_loss_mean = prediction_loss_epoch / num_train_examples
         linearity_loss_mean = linearity_loss_epoch / num_train_examples
 
         total_loss_mean = reconstruction_loss_mean + LAMBDA_LIN*linearity_loss_mean# + prediction_loss_mean
@@ -151,33 +146,37 @@ def train_model(model, data_loader, lifting_data_loader, num_epochs):
         if epoch >= RECON_EPOCH_THRESHOLD:
             scheduler.step(total_loss_mean)
 
-        train_history["loss_reconstruction"][epoch] = reconstruction_loss_epoch / num_train_examples
-        #train_history["loss_prediction"][epoch] = prediction_loss_epoch / num_train_examples
-        train_history["loss_linearity"][epoch] = linearity_loss_epoch / num_train_examples
+        linearity_estimation_error = total_linearity_error - irreducible_linearity_error
+        estimation_error_portion = linearity_estimation_error/total_linearity_error
+
+        train_history["loss_reconstruction"][epoch] = reconstruction_loss_mean
+        train_history["loss_linearity"][epoch] = linearity_loss_mean
+        train_history["irreducible_lin_error"][epoch] = irreducible_linearity_error
+        train_history["estimation_lin_error"][epoch] = linearity_estimation_error
         train_history["lr"][epoch] = scheduler.get_last_lr()[0]
 
         # eigenvalues
         with torch.no_grad():
-            K = model.linear_dynamics.weight.cpu().detach().numpy()
+            if model_type == "dmd":
+                K = lin_weights
+            else:
+                K = model.linear_dynamics.weight.cpu().detach().numpy()
             train_history["K"][epoch] = K
-
-
-        linearity_estimation_error = total_linearity_error - true_latent_linearity_error
-        estimation_error_portion = linearity_estimation_error/total_linearity_error
-
 
         epoch_progress.set_description("L(total): "+"{:.1e}".format(total_loss_mean)+", L(recon): "+"{:.1e}".format(reconstruction_loss_mean)+", L(lin): "+"{:.1e}".format(linearity_loss_mean)+", true_lin: "+"{:.1e}".format(true_latent_linearity_error)+", lin_est: "+"{:.1e}".format(linearity_estimation_error)+f"({estimation_error_portion*100:.0f}%), lr: " + "{:.1e}".format(scheduler.get_last_lr()[0]))
     return model, train_history
     
-LATENT_DIMENSION = 32
+LATENT_DIMENSION = 32 #[512, 128, 32]
 EPOCHS = 300
-RECON_EPOCH_THRESHOLD = 0
-LR = 4e-4
-LAMBDA_LIN = 100
+#RECON_EPOCH_THRESHOLD = 5
+LR = 2e-4
+LAMBDA_LIN = 1 # [1, 10]
+USEBIAS = True
+NORMTYPE = "batch" #["batch", "layer"]
+model_type = "train" #["train", "dmd", "lrgroup"]
 
-model_type = "dmd"#["train", "dmd", "lrgroup"]
-
-RUN_NAME = f"_L{LATENT_DIMENSION}_e{EPOCHS}_lr4e-4_lamlin{LAMBDA_LIN}_{model_type}"
+use_bias_label = "bias" if USEBIAS else "nobias"
+RUN_NAME = f"_L{LATENT_DIMENSION}_{use_bias_label}_norm{NORMTYPE}_lamlin{LAMBDA_LIN}_{model_type}"
 
 if __name__ == '__main__':
     # making a new folder to save the script and the results 
@@ -212,16 +211,16 @@ if __name__ == '__main__':
             lifting_dataset = SimpleDataset(train_data)
             lifting_loader = DataLoader(lifting_dataset, batch_size=64, shuffle=False, num_workers=4, pin_memory=True, persistent_workers=True)
 
-            model = ConvAutoencoderZeroDecoder(latent_dim=LATENT_DIMENSION).to(DEVICE)
+            model = ConvAutoencoder(latent_dim=LATENT_DIMENSION, bias_terms=USEBIAS, norm_type=NORMTYPE).to(DEVICE)
 
             if model_type == "lrgroup":
                 coder_params = [p for name, p in model.named_parameters() if 'coder' in name]
                 koopman_params = [p for name, p in model.named_parameters() if 'linear' in name]
             
                 optimizer = torch.optim.Adam([
-                    {'params': koopman_params, 'lr': LR*2},
-                    {'params': coder_params}
-                ], lr=LR)
+                    {'params': coder_params, 'lr': LR},
+                    {'params': koopman_params, 'lr': LR*2}
+                ])
             else:
                 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
@@ -229,14 +228,11 @@ if __name__ == '__main__':
 
             torch.save(trained_model.state_dict(), os.path.join(folder_name, "model.pt"))
             
-            history_keys = ["loss_reconstruction", "loss_linearity", "lr"]
+            history_keys = ["loss_reconstruction", "loss_linearity", "irreducible_lin_error", "estimation_lin_error", "lr"]
             losses_history = np.stack([train_history[key] for key in history_keys], axis=1)
             pd.DataFrame(losses_history, columns=history_keys).to_csv(os.path.join(folder_name,"losses_history.csv"), index = False)
 
-            np.save(os.path.join(folder_name,"lifted_state_history"), train_history["lifted_states"])
             np.save(os.path.join(folder_name,"K"), train_history["K"])
-            #pd.DataFrame(train_history["lifted_states"].squeeze(-1)).to_csv(os.path.join(folder_name,"lifted_state_history.csv"), index = False)
-            #pd.DataFrame(train_history["K"].squeeze(-1).squeeze(-1)).to_csv(os.path.join(folder_name,"K_history.csv"), index = False)
 
         except:
             traceback.print_exc(file=tb_file)
